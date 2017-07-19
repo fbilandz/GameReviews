bb9a6f42fdbbca468d46ec30242a309869e701a1
drGreenthumb1A4 <filip.pilot@gmail.com>
2017-07-19 15:23:22 +0200

    Djento, fu

--- extensions/hashtab.games/app/screens/MyReviews.js
+++ extensions/hashtab.games/app/screens/MyReviews.js
@@ -6,15 +6,63 @@ import {
   StyleSheet,
   Text,
   View,
-} from 'react-native';\
-import { ListView } from '@shoutem/ui'
+  TouchableOpacity,
+} from 'react-native';
+import { ListView } from '@shoutem/ui';
+import { connect } from 'react-redux';
+import { ext } from '../const';
+import _ from 'lodash';
+import { Review } from '../components/Review';
 
-export default class MyReviews extends Component {
-  render() {
+export class MyReviews extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      tried: false,
+    };
+    this.getMyReviews = this.getMyReviews.bind(this);
+    this.renderRow = this.renderRow.bind(this);
+    // this.getMyReviews();
+  }
+  componentWillMount() {
+    this.getMyReviews();
+  }
+  getMyReviews() {
+    const { reviews, userId } = this.props;
+    const s = [];
+    _.keys(reviews).map((value, index) => {
+      _.keys(reviews[value]).map((data, i) => {
+        if (reviews[value][data].userId === userId) s.push(reviews[value][data]);
+      });
+    });
+
+    console.log("gettam");
+    this.setState({
+      data: s,
+      tried: true,
+    });
+  }
+  edit() {
+
+  }
+  renderRow(data, id) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello World!</Text>
-      </View>
+      <TouchableOpacity onPress={this.edit}>
+        <Review data={data} key={id} />
+      </TouchableOpacity>);
+  }
+  render() {
+    const { tried, data } = this.state;
+    if (!tried) this.getMyReviews();
+    return (
+
+      (data !== undefined || data !== null) ?
+        <ListView
+          data={data}
+          renderRow={this.renderRow}
+        /> : <Text>No reviews yet</Text>
+
     );
   }
 }
@@ -29,3 +77,14 @@ const styles = StyleSheet.create({
     fontSize: 20,
   },
 });
+
+const mapStateToProps = (state) => {
+  const { reviews } = state[ext()];
+  const userId = _.get(state, ['shoutem.auth', 'user', 'id']);
+  return {
+    reviews,
+    userId,
+  };
+};
+
+export default connect(mapStateToProps, null)(MyReviews);
