8c191d5b1693e830a341b52a93e56a17841a4912
dinogrgic1 <dino.grgic1@gmail.com>
2017-07-20 11:30:53 +0200

    Components added in ReviewLayoutScreen.js

---
+++ extensions/hashtab.games/app/components/GameBanner.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { connectStyle } from '@shoutem/theme';
+import {
+    Title,
+    Caption,
+    Icon,
+    Tile,
+    Image,
+    View,
+} from '@shoutem/ui';
+import moment from 'moment';
+import { ext } from '../const';
+import { connect } from 'react-redux';
+
+export class GameBanner extends React.PureComponent {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <Image
+                styleName="large-banner placeholder"
+                source={this.props.articleImage}
+                animationName="hero"
+            >
+                <Tile animationName="hero">
+                    <Title styleName="centered">{this.props.article.title.toUpperCase()}</Title>
+                    {/* Virtual prop makes View pass Tile color style to Caption */}
+                    <View styleName="horizontal md-gutter-top" virtual>{}
+                        <Caption styleName="collapsible" numberOfLines={1}>{this.props.article.newsAuthor}</Caption>
+                        <Caption styleName="md-gutter-left">
+                            {moment(this.props.article.timeUpdated).fromNow()}
+                        </Caption>
+                    </View>
+                </Tile>
+                <Icon name="down-arrow" styleName="scroll-indicator" />
+            </Image>
+        );
+    }
+}
+
+export default connect()(connectStyle(ext('GameBanner'))(GameBanner)
+);
